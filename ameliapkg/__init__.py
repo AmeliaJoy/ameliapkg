5ff5ac8c10dde61751dbcedd0397632e08c727f3
Amelia Schroeder <ameliaschroeder@frc4322.com>
2022-08-10 13:56:07 -0700

    please push

---
+++ ameliapkg/__init__.py
@@ -0,0 +1,12 @@
+"""
+your package.
+
+info about your package.
+"""
+
+__version__ = "0.1.0"
+__author__ = 'Amelia Schroeder'
+__credits__ = 'Chillcrest Ave'
+
+def myversion():
+    print(__version__)
---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='ameliapkg',
+    version='0.1.0',    
+    description='Python test for amelia',
+    url='https://github.com/<<rest of url>>',
+    author='Amelia Schroeder',
+    author_email='amelia.schroeder@gmail',
+    packages=['ameliapkg'],
+    install_requires=['python-magic==0.4.27',
+                      ],
+
+    classifiers=[
+        'Operating System :: POSIX :: Linux',        
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+)
+
