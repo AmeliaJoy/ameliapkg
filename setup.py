e2ae3de31ec5452da55a610c85947e69567e752c
Amelia Schroeder <ameliaschroeder@frc4322.com>
2022-08-10 14:00:03 -0700

    version update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='ameliapkg',
-    version='0.1.0',    
+    version='0.1.1',    
     description='Python test for amelia',
     url='https://github.com/<<rest of url>>',
     author='Amelia Schroeder',
